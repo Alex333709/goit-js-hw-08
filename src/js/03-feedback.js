ae25b960f0d61218e53e6f9e9023aa68e84c21fc
Oleksandr Voloshun <voloshun3337@gmail.com>
2023-07-15 11:15:10 +0200

    work08

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -12,34 +12,20 @@ const updateFormState = throttle(() => {
     message: messageInput.value,
   };
 
-  try {
-    localStorage.setItem(formStateKey, JSON.stringify(formState));
-  } catch (error) {
-    console.error('Error saving form state:', error);
-  }
+  localStorage.setItem(formStateKey, JSON.stringify(formState));
 }, 500);
 
 const populateFormFields = () => {
   const savedFormState = localStorage.getItem(formStateKey);
-
   if (savedFormState) {
-    try {
-      const { email, message } = JSON.parse(savedFormState);
-      emailInput.value = email;
-      messageInput.value = message;
-    } catch (error) {
-      console.error('Error retrieving form state:', error);
-    }
+    const { email, message } = JSON.parse(savedFormState);
+    emailInput.value = email;
+    messageInput.value = message;
   }
 };
 
 const clearFormState = () => {
-  try {
-    localStorage.removeItem(formStateKey);
-  } catch (error) {
-    console.error('Error clearing form state:', error);
-  }
-
+  localStorage.removeItem(formStateKey);
   emailInput.value = '';
   messageInput.value = '';
 };
@@ -48,14 +34,11 @@ feedbackForm.addEventListener('input', updateFormState);
 window.addEventListener('DOMContentLoaded', populateFormFields);
 feedbackForm.addEventListener('submit', event => {
   event.preventDefault();
-
   const formState = {
     email: emailInput.value,
     message: messageInput.value,
   };
-
   console.log('Form submission:');
   console.log(formState);
-
   clearFormState();
 });
